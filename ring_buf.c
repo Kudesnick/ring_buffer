2d41f5f7c830f36b8bb5680ea1fd84e802e103e4
Stulov Tikhon <kudesnick@inbox.ru>
2023-02-02 16:59:50 +0300

    fix for asynchronous access

--- ring_buf.c
+++ ring_buf.c
@@ -5,16 +5,18 @@ r_size_t ring_get_free(t_ring *ring)
   return RING_BUF_SIZE - ring->w_pos + ring->r_pos;
 }
 
-r_data_t* ring_put_byte(t_ring *ring, r_data_t data) {
-  if (ring->w_pos - ring->r_pos == RING_BUF_SIZE) return 0;
-  r_data_t *ret = &ring->data[ring->w_pos++ % RING_BUF_SIZE];
-  *ret = data;
-  return ret;
+bool ring_put_byte(t_ring *ring, r_data_t data) {
+  if (ring->w_pos - ring->r_pos == RING_BUF_SIZE) return false;
+  ring->data[ring->w_pos % RING_BUF_SIZE] = data;
+  ring->w_pos++;
+  return true;
 }
 
-r_data_t* ring_get_byte(t_ring *ring) {
-  if (ring->w_pos == ring->r_pos) return 0;
-  return &ring->data[ring->r_pos++ % RING_BUF_SIZE];
+bool ring_get_byte(t_ring *ring, r_data_t *_data) {
+  if (ring->w_pos == ring->r_pos) return false;
+  *_data = ring->data[ring->r_pos % RING_BUF_SIZE];
+  ring->r_pos++;
+  return true;
 }
 
 r_size_t ring_put_data(t_ring *ring, r_data_t *data, r_size_t size) {
@@ -24,12 +26,8 @@ r_size_t ring_put_data(t_ring *ring, r_data_t *data, r_size_t size) {
 }
 
 r_size_t ring_get_data(t_ring *ring, r_data_t *data, r_size_t size) {
-  r_data_t *data_byte;
-  for (int i = 0; i < size; i++) {
-    data_byte = ring_get_byte(ring);
-    if (data_byte == 0) return i;
-    data[i] = *data_byte;
-  }
+  for (int i = 0; i < size; i++)
+	if (!ring_get_byte(ring, &data[i])) return i;
   return size;
 }
 
--- ring_buf.h
+++ ring_buf.h
@@ -1,11 +1,12 @@
 #include <stdint.h>
+#include <stdbool.h>
 
 #pragma once
 
 #define RING_BUF_SIZE 64
 
 typedef uint8_t r_data_t;
-typedef uint16_t r_size_t;
+typedef uint8_t r_size_t;
 
 typedef struct {
   r_size_t w_pos;
@@ -14,8 +15,8 @@ typedef struct {
 } t_ring;
 
 r_size_t ring_get_free(t_ring *ring);
-r_data_t* ring_put_byte(t_ring *ring, r_data_t data);
-r_data_t* ring_get_byte(t_ring *ring);
+bool ring_put_byte(t_ring *ring, r_data_t data);
+bool ring_get_byte(t_ring *ring, r_data_t *_data);
 r_size_t ring_put_data(t_ring *ring, r_data_t *data, r_size_t size);
 r_size_t ring_get_data(t_ring *ring, r_data_t *data, r_size_t size);
 void ring_flush(t_ring *ring);
